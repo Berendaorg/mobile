ec97884af5947075feca4512b9d098be45d017b0
tibex88 <tibesolomon7@gmail.com>
2024-09-07 23:53:34 +0300

    add: profile

--- app/(screens)/Profile.js
+++ app/(screens)/Profile.js
@@ -1,14 +1,34 @@
 import React from "react";
-import { Image, Text } from "react-native";
+import { Image, Text, View } from "react-native";
 
 const Profile = () => {
   return (
     <>
-      <Text>home</Text>
+    <View className="h-full w-full pt-3  flex-col gap-6 justify-start items-center">
       <Image
-        className="w-full h-full"
-        source={"./../../assets/images/home1.jpg"}
-      />
+        source={{
+          uri: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png",
+        }}
+        className="w-[100px] h-[100px] rounded-full">
+        </Image>
+      
+      <View className="flex flex-row justify-around w-full">
+        <Text className="rounded-lg pt-3 px-1 font-bold border border-black items-center p-3">
+          First Name
+        </Text>
+        <Text className="rounded-lg pt-3 px-1 font-bold border border-black items-center p-3">
+          Last Name
+        </Text>
+      </View>
+
+        <Text className="rounded-lg font-bold border border-black items-center p-3 w-full text-center">
+        Email
+        </Text>
+      
+        <Text className="rounded-lg font-bold border border-black items-center p-3 w-full text-center">
+          +2519547543
+        </Text>
+    </View>
     </>
   );
 };
