82a7db4cad76c21d705b45669c3972db674d77f2
tibex88 <tibesolomon7@gmail.com>
2024-09-26 23:35:58 +0300

    refactor to new api

---
+++ slices/apiSlice.js
@@ -0,0 +1,38 @@
+import { createApi } from '@reduxjs/toolkit/query/react'
+import { fetch } from '../mocks/fetch'
+
+
+const URL = 'https://fake-api.example.com/api/v1/';
+const mockFetchBaseQuery =
+  ({ baseUrl } = { baseUrl: '' }) =>
+  async ({ url, options }) => {
+    try {
+      const result = await fetch({
+        url: baseUrl + url,
+        options:{
+        method,
+        data,
+        params,
+        headers,
+        }
+      })
+      return { data: result.data }
+    } catch (error) {
+      const err = error
+      return {
+        error: {
+          status: err.response?.status,
+          data: err.response?.data || err.message,
+        },
+      }
+    }
+  }
+
+export const apiSlice = createApi({
+    reducerPath: 'api', // optional
+    baseQuery: mockFetchBaseQuery({
+        baseUrl: URL,
+      }),
+      tagTypes: ['Listing', 'Developer'],
+    endpoints: builder => ({})
+})
--- store.js
+++ store.js
@@ -6,13 +6,19 @@ import userReducer from './slices/userSlice'
 import developerReducer from './slices/developerSlice'
 import locationReducer from './slices/locationSlice'
 
+import { apiSlice } from '../features/api/apiSlice';
+
 export const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     location:locationReducer,
     search:searchReducer,
     notification:notificationReducer,
     developer:developerReducer,
     user: userReducer,
     listing:listingReducer,
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
   },
 })
